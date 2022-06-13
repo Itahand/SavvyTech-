cab0b2aa523e98553254b9708c84c448a6e8684c
Itahand Naizir <itahand.naizir@gmail.com>
2022-06-13 15:13:54 -0500

    Basic mobile layout.

--- savvytech/src/App.svelte
+++ savvytech/src/App.svelte
@@ -1,30 +1,86 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!!!!!!!!!!!!!!!!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header class='topbar' aria-label="Intro Section">
+		<h1>SAVVY TECH, LLC.</h1>
+	</header>
+
+	<div class="intro">
+		<div class="paragraph">
+			<h1>SAVVY TECH</h1>
+			<h3>Your Satisfaction is Our Priority</h3>
+			<p>Do you have something that needs fixing? Savvy Tech is here for all your repair needs. When possible, we use original parts for repairs, so anything you bring to us will be returned to you in mint condition. Feel free to pick up the phone with any questions you may have, we're invested in customer service and are always happy to help.</p>
+		</div>
+		<div class="picture">
+			<img src="https://static.wixstatic.com/media/2f3f25e96f1643c2b3543396ab016692.jpg/v1/fill/w_556,h_995,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/2f3f25e96f1643c2b3543396ab016692.jpg" alt="keyboard img"/>
+		</div>
+	</div>
+
+	<div class="services">
+		<h1>OUR SERVICES</h1>
+		<div class="div-section">
+			<div class="paragraph">
+				<h3>LAPTOP REPAIR</h3>
+				<p>This is one of our most popular services for both new and returning clients, so appointments fill up fast. Feel free to reach out with any questions for our team, and make sure to book your slot today.</p>
+			</div>
+			<div class="picture">
+				<img src="https://static.wixstatic.com/media/936c63273737da67239303dbf2bcd0bf.jpg/v1/fill/w_880,h_460,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/936c63273737da67239303dbf2bcd0bf.jpg" alt="keyboard img"/>
+			</div>
+		</div>
+		<div class="div-section">
+			<div class="paragraph">
+				<h3>LAPTOP REPAIR</h3>
+				<p>This is one of our most popular services for both new and returning clients, so appointments fill up fast. Feel free to reach out with any questions for our team, and make sure to book your slot today.</p>
+			</div>
+			<div class="picture">
+				<img src="https://static.wixstatic.com/media/936c63273737da67239303dbf2bcd0bf.jpg/v1/fill/w_880,h_460,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/936c63273737da67239303dbf2bcd0bf.jpg" alt="keyboard img"/>
+			</div>
+		</div>
+		<div class="div-section">
+			<div class="paragraph">
+				<h3>LAPTOP REPAIR</h3>
+				<p>This is one of our most popular services for both new and returning clients, so appointments fill up fast. Feel free to reach out with any questions for our team, and make sure to book your slot today.</p>
+			</div>
+			<div class="picture">
+				<img src="https://static.wixstatic.com/media/936c63273737da67239303dbf2bcd0bf.jpg/v1/fill/w_880,h_460,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/936c63273737da67239303dbf2bcd0bf.jpg" alt="keyboard img"/>
+			</div>
+		</div>
+		<div class="div-section">
+			<div class="paragraph">
+				<h3>LAPTOP REPAIR</h3>
+				<p>This is one of our most popular services for both new and returning clients, so appointments fill up fast. Feel free to reach out with any questions for our team, and make sure to book your slot today.</p>
+			</div>
+			<div class="picture">
+				<img src="https://static.wixstatic.com/media/936c63273737da67239303dbf2bcd0bf.jpg/v1/fill/w_880,h_460,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/936c63273737da67239303dbf2bcd0bf.jpg" alt="keyboard img"/>
+			</div>
+		</div>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.topbar {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  overflow: hidden;
+  padding-top: 0;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.intro {
+		background-color: #eaf5f7;
+		padding: 1rem 0;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.paragraph {
+		margin: 0 1rem;
+	}
+	.picture {
+		height: 400px;
+		width: 100%;
+	}
+	.picture > img {
+		width: 100%;
+		height: 100%;
+		margin: 1rem 0;
 	}
 </style>
